8252a2a5acb50d8c24e62cf15c0a807d1899dd25
ludvigtj <202105056@post.au.dk>
2023-04-14 12:56:49 +0200

    Testproject added

---
+++ TemperatureControl.Tests.Unit/Template.cs
@@ -0,0 +1,16 @@
+using nanoFramework.TestFramework;
+using System;
+
+//FOR COPY PASTE PURPOSES - DO NOT EDIT
+namespace TemperatureControl.Tests.Unit
+{
+    [TestClass]
+    public class Template
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
