e49e1517a621c1e94f99481a32b111bdfaa5fcfb
Cath20201 <78859065+Cath20201@users.noreply.github.com>
2023-04-14 11:01:26 +0200

    added RelayControl project

---
+++ RelayControl/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RelayControl
+{
+    public class RelayControl
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
