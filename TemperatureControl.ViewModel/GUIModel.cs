7908241732f499017f7bd2606699767f1c872e6f
Cath20201 <78859065+Cath20201@users.noreply.github.com>
2023-04-14 10:01:45 +0200

    Kommentar i GUIModel

--- TemperatureControl.ViewModel/GUIModel.cs
+++ TemperatureControl.ViewModel/GUIModel.cs
@@ -7,5 +7,6 @@ namespace TemperatureControl.ViewModel
         // Browse our samples repository: https://github.com/nanoframework/samples
         // Check our documentation online: https://docs.nanoframework.net/
         // Join our lively Discord community: https://discord.gg/gCyBu8T
+        // hejsa
     }
 }
