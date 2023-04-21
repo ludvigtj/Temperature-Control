d30c025ad3bd95446a982ef8717a4dce4cc3a80a
Lærke Kyed <202109223@post.au.dk>
2023-04-21 10:32:13 +0200

    Relay setup draft

--- RelayControl/RelayControl.cs
+++ RelayControl/RelayControl.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Iot.Device.Relay;
+using System.Device.I2c;
+using System.Threading;
+using nanoFramework.Hardware.Esp32;
 
 namespace RelayControl
 {
@@ -7,5 +11,13 @@ namespace RelayControl
         // Browse our samples repository: https://github.com/nanoframework/samples
         // Check our documentation online: https://docs.nanoframework.net/
         // Join our lively Discord community: https://discord.gg/gCyBu8T
+
+        public RelayControl()
+        {
+            Configuration.SetPinFunction(21, DeviceFunction.I2C1_DATA);
+            Configuration.SetPinFunction(22, DeviceFunction.I2C1_CLOCK);
+        }
+
+        
     }
 }
