545d9fb89b00f1e1ccb5ea3765399d95cd315f0a
ludvigtj <202105056@post.au.dk>
2023-04-21 11:18:41 +0200

    Concrete touchhandler

---
+++ TemperatureControl.ViewModel/IViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TemperatureControl.ViewModel
+{
+    internal interface IViewModel
+    {
+        public void Initialize();
+        public void FillButton_Pressed(object sender, EventArgs e);
+
+        public void EmptyButton_Pressed(object sender, EventArgs e);
+
+        public void RegulateButton_Pressed(object sender, EventArgs e);
+
+
+    }
+}
---
+++ TemperatureControl.ViewModel/MenuTouchHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using nanoFramework.Tough;
+
+namespace TemperatureControl.ViewModel
+{
+    internal class MenuTouchHandler: ITouchHandler
+    {
+        public void TouchEventCallback(object sender, TouchEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
