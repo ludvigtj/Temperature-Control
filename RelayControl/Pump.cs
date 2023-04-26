dc5af2cf43d906c73ad1037ba666105d21d0caf1
TrilleKongsbach <92532851+TrilleKongsbach@users.noreply.github.com>
2023-04-26 08:54:17 +0200

    Pump-klasse tilføjet

---
+++ RelayControl/Pump.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RelayControl.Interfaces;
+
+namespace RelayControl
+{
+    public class Pump : IPump
+    {
+        private readonly RelayControl _relay;
+
+        public Pump(RelayControl relay)
+        {
+            _relay = relay;
+        }
+        public void TurnOnPump()
+        {
+            _relay.TurnOnRelay(1);
+        }
+
+        public void TurnOffPump()
+        {
+            _relay.TurnOffRelay(1);
+        }
+    }
+}
