79dfdfed8678cda266726438b636e37ce8f8ba59
TrilleKongsbach <92532851+TrilleKongsbach@users.noreply.github.com>
2023-04-26 11:26:26 +0200

    ,

--- TemperatureControl.Tests.Unit/TestsForRelayControl/TemperatureRegulatorTests.cs
+++ TemperatureControl.Tests.Unit/TestsForRelayControl/TemperatureRegulatorTests.cs
@@ -1,28 +1,12 @@
 ﻿using nanoFramework.TestFramework;
 using RelayControl;
-using NSubstitube;
 
 namespace TemperatureControl.Tests.Unit.RelayControl
 {
     [TestClass]
     public class TemperatureRaegulatorTests
     {
-        [TestMethod]
-        public void Regulate_TurnsOnRelay_WhenActualTempIsBelowSetPointTemp()
-        {
-            // Arrange
-            var relayMock = Substitube.For<RelayController>();
-            var tempRegulator = new TemperatureRegulator(relayMock);
 
-            // Act
-            tempRegulator.SetPointTemp = 37;
-            tempRegulator.Regulate(34);
 
-            // Assert
-            relayMock.Received().TurnOnRelay(2);
-
-        }
-
-        
     }
 }
