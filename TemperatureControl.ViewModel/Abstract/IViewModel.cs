40ea20ab23bb87ffc0b7895e69ad34ecabd8c5aa
ludvigtj <113110940+ludvigtj@users.noreply.github.com>
2023-04-26 10:41:55 +0200

    viewmodel interface

--- TemperatureControl.ViewModel/Abstract/IViewModel.cs
+++ TemperatureControl.ViewModel/Abstract/IViewModel.cs
@@ -6,6 +6,16 @@ namespace TemperatureControl.ViewModel.Abstract
     {
         public void Initialize();
 
+        public void OnArrow_Pressed(object sender, EventArgs e);
+
+        public void OnSetPointPlu_Pressed(object sender, EventArgs e);
+        public void OnSetPointMinus_Pressed(object sender, EventArgs e);
+        public void OnFill_Pressed(object sender, EventArgs e);
+        public void OnEmpty_Pressed(object sender, EventArgs e);
+        public void OnRegulate_Pressed(object sender, EventArgs e);
+
+
+
 
     }
 }
