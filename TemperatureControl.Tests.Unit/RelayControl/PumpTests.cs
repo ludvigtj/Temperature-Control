2ede3f09f0dca536339faae977d49340e306dd0e
TrilleKongsbach <92532851+TrilleKongsbach@users.noreply.github.com>
2023-04-26 09:18:13 +0200

    PumpTests oprettet

---
+++ TemperatureControl.Tests.Unit/RelayControl/PumpTests.cs
@@ -0,0 +1,14 @@
+﻿using nanoFramework.TestFramework;
+
+namespace TemperatureControl.Tests.Unit.RelayControl
+{
+    [TestClass]
+    public class PumpTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
