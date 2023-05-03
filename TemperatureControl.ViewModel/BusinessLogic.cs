9d5248de863db246dea42506350728060145e9df
Mikkel Bjerre-Hansen <au668875@uni.au.dk>
2023-05-03 11:26:47 +0200

    Implemented NotifyPropertyChanged() and fixing BusinessLogic

--- TemperatureControl.ViewModel/BusinessLogic.cs
+++ TemperatureControl.ViewModel/BusinessLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using nanoFramework.UI;
+using System;
 using System.Threading;
 using TemperatureControl.RelayControl.Interfaces;
 using TemperatureSensor.Interfaces;
@@ -11,64 +12,88 @@ namespace TemperatureControl.ViewModel
         private ITemperatureSensor _tempSensor;
         private IPump _pump;
         private ITemperatureRegulator _tempRegulator;
+        private IValve _tabValve;
+        private IValve _tubValve;
 
-        public BusinessLogic(IRelayController relayController,ITemperatureSensor tempSensor, IPump pump, ITemperatureRegulator tempRegulator)
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _temperature;
+        public double Temperature
         {
-            
+            get
+            {
+                return _temperature;
+            }
+            set
+            {
+                if (value != _temperature)
+                {
+                    _temperature = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        private void NotifyPropertyChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature), Temperature, _temperature));
+        }
+
+        public BusinessLogic(IRelayController relayController,ITemperatureSensor tempSensor, IPump pump, ITemperatureRegulator tempRegulator, IValve tabValve, IValve tubValve)
+        {
+            _relayController = relayController;
+            _tempSensor = tempSensor;
+            _pump = pump;
+            _tempRegulator = tempRegulator;
+            _tabValve = tabValve;
+            _tubValve = tubValve;
         }
 
         public void FillVessel(double setPointTemp)
         {
-            //_tabValve.OpenValve();
-            //_tubValve.OpenValve();
+            _tabValve.OpenValve();
+            _tubValve.OpenValve();
+
             Thread.Sleep(300000); // Fylder i 5 minutter
             //Use stopwatch
 
             for (int i = 0; i < 10; i++) // lukker og åbner ventil i intervaller af 1 sekund i 10 sekunder
             {
-                //_tabValve.CloseValve(); - These members are not a part of the interface
-                //Thread.Sleep(1000);
-                //i++;
-                //_tabValve.OpenValve(); 
-                //Thread.Sleep(1000);
+                _tabValve.CloseValve();
+                Thread.Sleep(1000);
+                i++;
+                _tabValve.OpenValve(); 
+                Thread.Sleep(1000);
             }
             _pump.TurnOnPump();
             _tempRegulator.Regulate(setPointTemp);
 
-            //Thread.Sleep(900000); // Holder ventil til brugsvand åben i 15 minutter
-
-            //_tabValve.CloseValve();
-
-            Timer timer = new Timer(TimerCallback, null, 5 * 60 * 60 * 1000, Timeout.Infinite);
-
+            
+            Timer timer = new Timer(TimerCallback, null, 15 * 60 * 1000, Timeout.Infinite); // 15 minutter
+            // Holder ventil til brugsvand åben i 15 minutter
             void TimerCallback(object state)
             {
-                //_tabValve.CloseValve();
+                _tabValve.CloseValve();
             }
 
             // ER I TVIVL OM NEDENSTÅENDE
 
-            Timer timer2 = new Timer(TimerCallback2, null, 5 * 60 * 60 * 1000, Timeout.Infinite);
+            Timer timer2 = new Timer(TimerCallback2, null, 5 * 60 * 60 * 1000, Timeout.Infinite); // 5 timer
 
             void TimerCallback2(object state)
             {
                 _pump.TurnOffPump();
-                //_tubValve.CloseValve();
+                _tubValve.CloseValve();
                 //_tempRegulator.StopRegulate(); //Sluk pumpe, temperaturregulering og luk ventil til karret efter 5 timer.
             }
         }
 
         public void EmptyVessel()
         {
-            //_tubValve.CloseValve();
+            _tubValve.CloseValve();
             _pump.TurnOnPump();
             //_tempRegulator.StopRegulate(); - Not in interface
 
-            //Thread.Sleep(1200000); // Skal tømme i 20 minutter. Kan dette bare løses sådan? 
-            //_pump.TurnOffPump();
-
-            Timer timer = new Timer(TimerCallback, null, 5 * 60 * 60 * 1000, Timeout.Infinite);
-
+            Timer timer = new Timer(TimerCallback, null, 20 * 60 * 1000, Timeout.Infinite); // 20 minutter
+            // Skal tømme i 20 minutter.
             void TimerCallback(object state)
             {
                 _pump.TurnOffPump();
@@ -78,31 +103,31 @@ namespace TemperatureControl.ViewModel
 
         public void RegulateTemperature(double setPointTemp)
         {
-            //_tubValve.OpenValve();
+            _tubValve.OpenValve();
             _pump.TurnOnPump();
             _tempRegulator.Regulate(setPointTemp);
 
-            Timer timer = new Timer(TimerCallback, null, 5 * 60 * 60 * 1000, Timeout.Infinite);
+            Timer timer = new Timer(TimerCallback, null, 5 * 60 * 60 * 1000, Timeout.Infinite); // 5 timer
 
             void TimerCallback(object state)
             {
-                //_tubValve.CloseValve();
+                _tubValve.CloseValve();
                 _pump.TurnOffPump();
-                //_tempRegulator.StopRegulate();
+                //_tempRegulator.StopRegulate(); - Not in interface
             }
         }
 
         public void CheckTemperature(double setPointTemp)
         {
-            //while (_tempSensor.ReadTemperature() < setPointTemp + 2 || _tempSensor.ReadTemperature() > setPointTemp -2 )
+            while (_tempSensor.ReadTemperature() < setPointTemp + 2 || _tempSensor.ReadTemperature() > setPointTemp -2 )
             {
-   
+
             }
 
-            //_tabValve.CloseValve();
-            //_tubValve.CloseValve();
+            _tabValve.CloseValve();
+            _tubValve.CloseValve();
             _pump.TurnOffPump();
-            //_tempRegulator.StopRegulate();
+            //_tempRegulator.StopRegulate(); - Not in interface
         }
     }
 }
