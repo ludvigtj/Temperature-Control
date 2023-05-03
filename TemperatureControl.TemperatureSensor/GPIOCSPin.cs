f0fe1b8bbb0c84b7e64f21e4493abcb786d11170
Mikkel Bjerre-Hansen <au668875@uni.au.dk>
2023-05-03 10:20:44 +0200

    Added GPIOCSPin class and Methods

---
+++ TemperatureControl.TemperatureSensor/GPIOCSPin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TemperatureSensor.Interfaces;
+using System.Device.Gpio;
+
+namespace TemperatureSensor
+{
+    public class GPIOCSPin : IGPIOCSPin
+    {
+        GpioPin csPin;
+
+        public GPIOCSPin()
+        {
+            csPin = new GpioController().OpenPin(27);
+        }
+
+        public void CreateCSPin()
+        {
+            csPin.SetPinMode(PinMode.Output);
+            csPin.Write(PinValue.High);
+        }
+
+        public void SelectCSPin()
+        {
+            csPin.Write(PinValue.Low);
+        }
+
+        public void DeselectCSPin()
+        {
+            csPin.Write(PinValue.High);
+        }
+    }
+}
