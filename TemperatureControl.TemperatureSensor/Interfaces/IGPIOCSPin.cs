2968ecb97f1dfd8306e3b69662a2fa7926bd49c1
Mikkel Bjerre-Hansen <au668875@uni.au.dk>
2023-05-03 09:47:25 +0200

    Added IGPIOCSPin

---
+++ TemperatureControl.TemperatureSensor/Interfaces/IGPIOCSPin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TemperatureSensor.Interfaces
+{
+
+    public interface IGPIOCSPin
+    {
+        void CreateCSPin();
+        void SelectCSPin();
+        void DeselectCSPin();
+
+    }
+}
+
+
+
