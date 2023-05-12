1625435cd29ec02c1c6ee7de60faa71915fcb245
Mikkel Bjerre-Hansen <au668875@uni.au.dk>
2023-05-12 09:34:47 +0200

    Added CurrentTemp to ITemperatureRegulator

--- TemperatureControl.RelayControl/Interfaces/ITemperatureRegulator.cs
+++ TemperatureControl.RelayControl/Interfaces/ITemperatureRegulator.cs
@@ -5,7 +5,8 @@ namespace TemperatureControl.RelayControl.Interfaces
     public interface ITemperatureRegulator
     {
         double SetPointTemp { get; set; }
-        void Regulate(double actualTemp);
+        double CurrentTemp { get; set; }
+        void Regulate();
         void StopRegulate();
     }
 }
